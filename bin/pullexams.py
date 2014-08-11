bab51ff27ee0d60f16b9be0add3663a9ce6f20a8
Jon Pipitone <jon@pipitone.ca>
2014-08-11 17:27:17 -0400

    Tidyied up pullexams.py

--- bin/pullexams.py
+++ bin/pullexams.py
@@ -1,69 +1,90 @@
 #!/bin/env python
 # vim: expandtab ts=4 sw=4 tw=80: 
+""" 
+Finds and copies exam data into a well-organized folder structure.
+
+Usage: 
+    mritool.py [options] [pull] [<examid>...]
+Options: 
+    -n, --dry-run           Do nothing. 
+    --output-dir=<dir>      Output directory [default: ./output]
+    --log-dir=<dir>         Logging directory [default: ./output/logs]
+"""
+
+from docopt import docopt
 import os
 import os.path
 import datetime 
 import dicom
+import glob 
+import sys
+DICOM_PRESSCI_KEY = 0x0019109e
+DICOM_PFILEID_KEY = 0x001910a2
 
-STAGING_DIR = "./data/scans"
-LOGS_DIR    = "./data/scans/logs"
+def pull_exams(arguments): 
+    output_dir = arguments['--output-dir']
+    log_dir    = arguments['--log-dir'] 
+    dry_run    = arguments['--dry-run']
+    examids    = arguments['<examid>']
 
-for line in os.popen("getallstudycodes | grep DTIG | tail -1"):     	
-    (examid, studycode, date, patient_id, patient_name) = line.split()
+    for line in os.popen("getallstudycodes"):     	
     
-    studydir    = STAGING_DIR +"/" + studycode
-    examdirname = "{studycode}_E{examid}_{date}_{patient_id}_{patient_name}" \
-                    .format(**vars())
-    examdir     = "{studydir}/{examdirname}".format(**vars())
-    exam_tarball= "{0}/{1}.tar.gz".format(studydir, examdirname)
-    examinfo    = "{0}/{1}.info".format(studydir, examdirname)
-    
-    print "Considering exam {0} from study {1}... ".format(
-        examid, studycode),
+        (examid, studycode, date, patient_id, patient_name) = line.strip('\n').split('\t')
 
-    # Create the study directory if it doesn't exist 
-    if not os.path.isdir(studydir): 
-        print "Directory {0} does not exist. Skipping.".format(studydir)
-  
-    if os.path.isfile(exam_tarball):  
-        print "skipping (because {0} exists).".format(exam_tarball)
-        continue
-    
-
-    print "processing."
-    print "---"
-    print "[{0}] Starting... ".format(datetime.datetime.now().isoformat(" "))
-
-    if not os.path.exists(examdir): 
-        print "Fetching DICOMS into {0}".format(examdir)
-        logfile = "{0}/{1}.getdicom.log".format(LOGS_DIR,examdirname)
-        if os.system("getdicom -d {examdir} {examid} > {logfile}".format(
-            **vars())):
-            print "Error getting exam {0} DICOMS.".format(examid)
+        if examids and examid not in examids:
             continue
-        os.system("examinfo {0} | tee {1} > {2}/exam_info.txt".format(
-            examid, examinfo, examdir))
-        os.system("listseries11 {0} | tee -a {1} > {2}/exam_info.txt".format(
-            examid, examinfo, examdir))
+        
+        studydir    = output_dir +"/" + studycode
+        examdirname = "{studycode}_E{examid}_{date}_{patient_id}_{patient_name}" \
+                        .format(**vars())
+        examdir     = "{studydir}/{examdirname}".format(**vars())
+        examinfo    = "{0}/{1}.info".format(studydir, examdirname)
+        
+        print "Considering exam {0} from study {1}... ".format(
+            examid, studycode),
 
-    # create tarfile and MD5sum
-    print "Creating tar file of {0} as {1}".format(examdir, exam_tarball)
-    tarcmd = "tar --remove-files -czf {0} -C {1} {2}".format(
-        exam_tarball, studydir, examdirname)
+        os.path.isdir(studydir) or os.makedirs(studydir) 
+        os.path.isdir(log_dir) or os.makedirs(log_dir) 
+     
+        print "checking.\n---" 
+        
+        # get dicoms
+        if not os.path.isdir(examdir):  
+            print "Fetching DICOMS into {0}".format(examdir)
+            logfile = "{0}/{1}.getdicom.log".format(log_dir,examdirname)
+            if os.system("getdicom -d {examdir} {examid} > {logfile}".format(
+                **vars())):
+                print "Error getting exam {0} DICOMS.".format(examid)
+                continue
+            os.system("examinfo {0} | tee {1} > {2}/exam_info.txt".format(
+                examid, examinfo, examdir))
+            os.system("listseries11 {0} | tee -a {1} > {2}/exam_info.txt".format(
+                examid, examinfo, examdir))
 
-    if os.system(tarcmd): 
-        print "Error creating tarball {0}".format(exam_tarball)
-        continue
-    os.system('echo "md5sum: $(md5sum {0})" >> {1}'.format(
-        exam_tarball, examinfo))
+        # get pfiles
+        for series_dir in glob.glob(examdir + "/E*"): 
+            for dcm_file in glob.glob(series_dir +"/*.dcm"): 
+                ds = dicom.read_file(dcm_file)
 
-    # set group read permissions
-    print "Setting correct permissions..."
-    chmodcmd = "chmod -R u=rwX,g=rX,o= {0} {1}".format(exam_tarball, examinfo)
+                if DICOM_PRESSCI_KEY not in ds or ds[DICOM_PRESSCI_KEY].value != "presscsi": 
+                    break 
 
-    if os.system(chmodcmd):
-        print "Error setting exam permissions."
-        continue
+                if DICOM_PFILEID_KEY not in ds: 
+                    print "Error.. pfile field not found"
+                    break 
 
-    print "[{0}] Finished. ".format(datetime.datetime.now().isoformat(" "))
-    print "---\n"
+                pfile_id = ds[DICOM_PFILEID_KEY].value
+                print "TODO: fetch pfile", pfile_id, "for series", series_dir
+                break
+            
+        
+        print "[{0}] Finished. ".format(datetime.datetime.now().isoformat(" "))
+        print "---\n"
+
+
+
+
+if __name__ == "__main__":
+    arguments = docopt(__doc__)
+    pull_exams(arguments)
+
