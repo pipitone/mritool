6d28c03cdc0ad194d431a859e4ed72dab5dc41b5
Jon Pipitone <jon@pipitone.ca>
2014-08-12 16:26:01 -0400

    Added PFile class

--- mritools/mritools.py
+++ mritools/mritools.py
@@ -7,28 +7,24 @@ import pfile_tools
 from pfile_tools import headers, struct_utils
 import sys
 
-default_pfile_headers = [ 
-    'exam_number',
-    'exam_description', 
-    'exam_type',
-    'series_number', 
-    'series_description', 
- ]
+class PFile: 
+    def __init__(self, path): 
+        self.path = path
+        self.headers = {}
+        
 
 def get_pfile_info(path): 
     """Returns a dictionary of header->values for a pfile.
 
 Returns None if the specified path does not point to valid pfile."""
 
-    pfile_headers = default_pfile_headers
     try: 
         ph = pfile_tools.headers.Pfile.from_file(path)
         dump = struct_utils.dump_struct(ph.header)
-        info = {}
+        pfile = PFile(path)
         for record in dump: 
-            if record.label in pfile_headers:
-                info[record.label] = record.value
-        return info
+            pfile.headers[record.label] = record.value
+        return pfile 
     except (IOError, headers.UnknownRevision): 
         return None
 
@@ -37,10 +33,10 @@ def get_all_pfiles_info(root):
 
 Returns a dictionary mapping the path of a pfile to attributes about the pfile.
 """
-    infos = {}
+    pfiles = {}
     for (path, dirs, files) in os.walk(root):
         for f in files: 
             full_path = os.path.join(path, f)
-            info = get_pfile_info(full_path)
-            if info: infos[full_path] = info
-    return infos
+            pfile = get_pfile_info(full_path)
+            if pfile: pfiles[full_path] = pfile 
+    return pfiles
--- tests/test_pfiles.py
+++ tests/test_pfiles.py
@@ -3,39 +3,42 @@ from mritools import mritools
 import unittest
 
 def test_get_pfile_info_nonexistant(): 
-    pfile = "tests/does-not-exist"
-    info = mritools.get_pfile_info(pfile)
-    assert info is None
+    path = "tests/does-not-exist"
+    pfile = mritools.get_pfile_info(path)
+    assert pfile is None
 
 def test_get_pfile_info_invalid_pfile(): 
-    pfile = "tests/test_pfiles.py"
-    info = mritools.get_pfile_info(pfile)
-    assert info is None
+    path = "tests/test_pfiles.py"
+    pfile = mritools.get_pfile_info(path)
+    assert pfile is None
 
 def test_get_pfile_info_valid_pfile(): 
-    pfile = "tests/valid-pfile.7"
-    info = mritools.get_pfile_info(pfile)
-    assert info is not None
-    assert info["exam_number"] == 2711
-    assert info["exam_description"] == "Daily QA" 
-    assert info["exam_type"] == "MR" 
-    assert info["series_number"] == 6
-    assert info["series_description"] == "AX FSPGR"
+    path = "tests/valid-pfile.7"
+    pfile = mritools.get_pfile_info(path)
+    assert pfile is not None
+    assert pfile.path == path
+    assert pfile.headers["exam_number"] == 2711
+    assert pfile.headers["exam_description"] == "Daily QA" 
+    assert pfile.headers["exam_type"] == "MR" 
+    assert pfile.headers["series_number"] == 6
+    assert pfile.headers["series_description"] == "AX FSPGR"
 
 def test_get_pfile_info_valid_hos_pfile(): 
-    pfile = "tests/valid-hos-pfile.7"
-    info = mritools.get_pfile_info(pfile)
-    assert info is not None
-    assert info["exam_number"] == 2713
-    assert info["exam_description"] == "ALCO1MR" 
-    assert info["exam_type"] == "MR" 
-    assert info["series_number"] == 4
-    assert info["series_description"] == "HOS"
+    path = "tests/valid-hos-pfile.7"
+    pfile = mritools.get_pfile_info(path)
+    assert pfile is not None
+    assert pfile.path == path
+    assert pfile.headers["exam_number"] == 2713
+    assert pfile.headers["exam_description"] == "ALCO1MR" 
+    assert pfile.headers["exam_type"] == "MR" 
+    assert pfile.headers["series_number"] == 4
+    assert pfile.headers["series_description"] == "HOS"
 
 def tests_get_all_pfiles_info(): 
     root = "tests/"
-    infos = mritools.get_all_pfiles_info(root)
-    assert len(infos) == 2
-    assert "tests/valid-hos-pfile.7" in infos
-    assert "tests/valid-pfile.7" in infos
-    assert infos["tests/valid-pfile.7"]["exam_number"] == 2711
+    pfiles = mritools.get_all_pfiles_info(root)
+    assert len(pfiles) == 2
+    assert "tests/valid-hos-pfile.7" in pfiles
+    assert "tests/valid-pfile.7" in pfiles
+    assert pfiles["tests/valid-pfile.7"].headers["exam_number"] == 2711
+    assert pfiles["tests/valid-pfile.7"].path == "tests/valid-pfile.7"
